467733cea215e3ea82ef02e811a8dcaa69e40144
James R Morey <jamesmorey316@gmail.com>
2016-09-10 12:14:02 -0700

    added Get-CodonCount and revised internal implementation details

---
+++ SequenceStudioPowerShell/Cmdlets/Get-CodonCount.cs
@@ -0,0 +1,26 @@
+﻿using System.Management.Automation;
+using SequenceStudio;
+
+namespace SequenceStudioPowerShell
+{
+    [Cmdlet(VerbsCommon.Get, "CodonCount")]
+    public class GetCodonCountCmdlet : PSCmdlet
+    {
+        [Parameter(Mandatory = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "Provide a valid SequenceStudio.DNA class instance.")]
+        [Alias("d")]
+        public DNA Dna
+        {
+            set { _dna = value; }
+        }
+        private DNA _dna;
+
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(SequenceMethods.CodonCount(_dna));
+        }
+    }
+}
